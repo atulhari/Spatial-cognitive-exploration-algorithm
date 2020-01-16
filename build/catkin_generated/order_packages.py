4c2d2ae5eb799e42afc6247e71cf63bad412215a
atulhari <atulhari3011@gmail.com>
2020-01-16 17:07:16 +0100

    launch updated

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/atul/Spatial-cognitive-exploration-algorithm/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/atul/Spatial-cognitive-exploration-algorithm/devel;/home/atul/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/atul/Spatial-cognitive-exploration-algorithm/devel;/home/atul/catkin_ws/devel;/opt/ros/kinetic" != "" else []
--- build/jackal/jackal_desktop/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/atul/Spatial-cognitive-exploration-algorithm/src/jackal/jackal_desktop
-# Build directory: /home/atul/Spatial-cognitive-exploration-algorithm/build/jackal/jackal_desktop
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
